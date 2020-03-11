3935149d7b5a3f507b9b59317f629046f00cd6fc
rain <xiayu@hoperun.com>
2020-03-11 19:28:42 +0800

    修改

---
+++ maven-1.3/src/main/java/asdlkjf.java
@@ -0,0 +1,44 @@
+import java.util.Date;
+
+public class asdlkjf {
+    public static String ear(){
+        ear1();
+        System.out.println("skjflakj");
+        Date date = new Date();
+        return "kjfsklj";
+    }
+    public static String ear5(){
+        System.out.println("skjflakj");
+        Date date = new Date();
+        return "kjfsklj";
+    }
+    public static String ear4(){
+        ear2();
+        System.out.println("skjflakj");
+        Date date = new Date();
+        return "kjfsklj";
+    }
+    public static String ear1(){
+        ear3();
+        System.out.println("skjflakj");
+        Date date = new Date();
+        return "kjfsklj";
+    }
+    public static String ear2(){
+        ear5();
+        System.out.println("skjflakj");
+        Date date = new Date();
+        return "kjfsklj";
+    }
+    public static String ear3(){
+        ear4();
+        System.out.println("skjflakj");
+        Date date = new Date();
+        return "kjfsklj";
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(ear());
+    }
+}
